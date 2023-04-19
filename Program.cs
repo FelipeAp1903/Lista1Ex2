c2b509004d92dcc5a6c50c7b5e905b9e05262141
FelipeAp1903 <131301360+FelipeAp1903@users.noreply.github.com>
2023-04-19 16:39:20 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista1ex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Double n1;
+            Double e;
+            Double soma;
+            Console.WriteLine("coloque o valor da aresta do quadrado:");
+            n1 = Double.Parse(Console.ReadLine());
+            e = n1;
+            soma = n1 * e;
+            Console.Write("o Valor da Área é: {0}", soma);
+            Console.WriteLine();
+
+        }
+    }
+}
